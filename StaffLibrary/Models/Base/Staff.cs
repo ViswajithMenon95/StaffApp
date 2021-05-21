e827bbc2061b306a3413d8be3e8130d15332096c
ViswajithMenon95 <viswajithmenon95@gmail.com>
2021-05-21 12:50:24 +0530

    Remove non-required methods from base model class

--- StaffLibrary/Models/Base/Staff.cs
+++ StaffLibrary/Models/Base/Staff.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace StaffLibrary.Models.Base
 {
@@ -11,36 +10,5 @@ namespace StaffLibrary.Models.Base
 		public string Type { get; set; }
 		public string Phone { get; set; }
 
-		public virtual void AddStaffDetails()
-		{
-			Console.WriteLine("Enter the staff name");
-			Name = Console.ReadLine();
-			Console.WriteLine("Enter the staff phone number");
-			Phone = Console.ReadLine();
-		}
-		public virtual void UpdateStaffDetails()
-		{
-			string checkInput;
-			Console.WriteLine("Enter the new staff name");
-			checkInput = Console.ReadLine();
-			if (checkInput != "")
-			{
-				Name = checkInput;
-			}
-
-			Console.WriteLine("Enter the new staff phone number");
-			checkInput = Console.ReadLine();
-			if (checkInput != "")
-			{
-				Phone = checkInput;
-			}
-		}
-		public virtual void ViewStaffDetails()
-		{
-			Console.WriteLine("Id: {0}", Id);
-			Console.WriteLine("Name: {0}", Name);
-			Console.WriteLine("Type: {0}", Type);
-			Console.WriteLine("Phone: {0}", Phone);
-		}
 	}
 }

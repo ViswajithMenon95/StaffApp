fd7d60bda059050cb918d4c5239971ea40dd648d
ViswajithMenon95 <viswajithmenon95@gmail.com>
2021-06-15 10:13:25 +0530

    Enable CORS for requests from other ports

--- WebApp/App_Start/WebApiConfig.cs
+++ WebApp/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebApp
 {
@@ -7,6 +8,9 @@ namespace WebApp
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
-        }
+
+			var cors = new EnableCorsAttribute("*", "*", "*");
+			config.EnableCors(cors);
+		}
     }
 }
